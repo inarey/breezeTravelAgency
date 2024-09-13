441dde19214de4487fac1eb5a919d02e88f9334d
shan-ti <thoselovelywords@gmail.com>
2024-09-13 12:00:35 +0900

    modified assets and types

--- frontend/src/components/header/Navbar.tsx
+++ frontend/src/components/header/Navbar.tsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom"
+import {breezeLogo} from "../../assets/imgs-brz/breezeLogo.png"
 
 const Navbar = () => {
   return (
     <>
       {/* Header Styles */}
-      <header className="w-full mx-auto flex items-center justify-between bg-primary-500">
-        <nav className="flex justify-between items-center sticky z-10 mx-auto py-4 bg-red-500">
+      <header className='w-full mx-auto flex items-center justify-between bg-primary-500'>
+        <nav className='flex justify-between items-center sticky z-10 mx-auto py-4 bg-red-500'>
           {/* Brand Logo Link to Home */}
+          <Link>
+            <img src={breezeLogo} alt='' />
+          </Link>
         </nav>
-
-
       </header>
     </>
-  )
+  );
 }
 export default Navbar
